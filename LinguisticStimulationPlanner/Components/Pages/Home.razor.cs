aa6057c97c2aa5aed4bb0fed6b2c1280f85f6019
Dwarf1er <SerolTech@protonmail.com>
2025-01-07 18:11:53 -0500

    feat: add home page cards for navigation

---
+++ LinguisticStimulationPlanner/Components/Pages/Home.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace LinguisticStimulationPlanner.Components.Pages
+{
+    public partial class Home
+    {
+        private int homeElevation = 2;
+        private int locationElevation = 2;
+        private int goalElevation = 2;
+        private int toyElevation = 2;
+        private int patientElevation = 2;
+        private int planElevation = 2;
+    }
+}
